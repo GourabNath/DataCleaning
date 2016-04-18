b642dd738f29a98a16c17eac427968204171d206
GourabNath <gourabnath88@gmail.com>
2016-04-18 20:31:53 +0500

    Included Comments and Variable Description

--- outLocate() function.R
+++ outLocate() function.R
@@ -1,5 +1,24 @@
-#Locating Outliers:
-#outLocate() function
+## Locating Outliers:
+## outLocate() function
+##
+## Outlier is definitely a matter of interest. Therefore it is required that we should know where exactly this outlier is located for a
+## particular variable. This function will help to do so.
+##
+## The function outLocate takes two arguments - 
+## data - a data frame of interest
+## bench - the benchmark.
+## There are four benchmarks used to construct this function:
+## b1 - Based on quartile and interquartile range. (Upper BM, Lower BM) = (Q1 - 1.5*IQR, Q3 + 1.5*IQR)
+## b2 - Based on mean and sd. (Upper BM, Lower BM) = (mean - 2*sd, mean + 2*sd)
+## b3 - Based on mean and sd. (Upper BM, Lower BM) = (mean - 2.5*sd, mean + 2.5*sd)
+## b4 - Based on mean and sd. (Upper BM, Lower BM) = (mean - 3*sd, mean + 3*sd)
+##
+## The finction returns a list. Each location of the list corresponds to a variable in the data frame.
+## If a particular variable of the data frame contains outliers then the corresponding location of the list returns a vector containing the index where 
+## in the variable the outlier is located.
+## If a particular variable of the data frame does not contain a outlier or a categorical variable then the corresponding location of the list
+## shows a hypen (indicating not applicable)
+
 
 outLocate <- function(data, benchmark = c("b1","b2","b3","b4"))
 {
@@ -25,31 +44,34 @@ outLocate <- function(data, benchmark = c("b1","b2","b3","b4"))
     
     for(i in 1:ncol(data))
     {
+      #Constructing the upper and lower benchmark
       Lbench = quantile(data[,i][!is.na(data[,i])], 0.25) - 1.5*IQR(data[,i][!is.na(data[,i])])/2
       Ubench = quantile(data[,i][!is.na(data[,i])], 0.75) + 1.5*IQR(data[,i][!is.na(data[,i])])/2
       
+      #Using which() function to locate the outliers
       a <- which(data[,i] > Ubench)
       b <- which(data[,i] < Lbench)
       
-      if(length(c(a,b)) > 0)
+      #Note the if there are no outlier then the length of the vector c(a,b) will be zero
+      if(length(c(a,b)) > 0)  #if outlier is present
       {
-        outLoc[[i]] <- c(a,b)
+        outLoc[[i]] <- c(a,b) #Then the ith location of the list is the vector c(a,b)
       }
-      else
+      else                    #Else If the outlier is not present
       {
-        outLoc[[i]] <- "-"
+        outLoc[[i]] <- "-"    #Then the ith location of the list is an hypen (indicating not applicable)
       }
     }
   }
       
       if(benchmark == "b2")
       {
-        #Here the benchmark used is (Q1 - 1.5*IQR, Q3 + 1.5*IQR)
+        #Here the benchmark used is (mean - 2*sd, mean + 2*sd)
         
         for(i in 1:ncol(data))
         {
-          Lbench = mean(data[,i][!is.na(data[,i])]) - 1.5*sd(data[,i][!is.na(data[,i])])
-          Ubench = mean(data[,i][!is.na(data[,i])]) + 1.5*sd(data[,i][!is.na(data[,i])])
+          Lbench = mean(data[,i][!is.na(data[,i])]) - 2*sd(data[,i][!is.na(data[,i])])
+          Ubench = mean(data[,i][!is.na(data[,i])]) + 2*sd(data[,i][!is.na(data[,i])])
           
           a <- which(data[,i] > Ubench)
           b <- which(data[,i] < Lbench)
@@ -70,12 +92,12 @@ outLocate <- function(data, benchmark = c("b1","b2","b3","b4"))
     
     if(benchmark == "b3")
     {
-      #Here the benchmark used is (Q1 - 2*IQR, Q3 + 2*IQR)
+      #Here the benchmark used is (mean - 2.5*sd, mean + 2.5*sd)
       
       for(i in 1:ncol(data))
       {
-        Lbench = mean(data[,i][!is.na(data[,i])]) - 2*sd(data[,i][!is.na(data[,i])])
-        Ubench = mean(data[,i][!is.na(data[,i])]) + 2*sd(data[,i][!is.na(data[,i])])
+        Lbench = mean(data[,i][!is.na(data[,i])]) - 2.5*sd(data[,i][!is.na(data[,i])])
+        Ubench = mean(data[,i][!is.na(data[,i])]) + 2.5*sd(data[,i][!is.na(data[,i])])
         
         a <- which(data[,i] > Ubench)
         b <- which(data[,i] < Lbench)
@@ -96,7 +118,7 @@ outLocate <- function(data, benchmark = c("b1","b2","b3","b4"))
     
     if(benchmark == "b4")
     {
-      #Here the benchmark used is (Q1 - 3*IQR, Q3 + 3*IQR)
+      #Here the benchmark used is (mean - 3*sd, mean + 3*sd)
       
       for(i in 1:ncol(data))
       {
@@ -119,7 +141,7 @@ outLocate <- function(data, benchmark = c("b1","b2","b3","b4"))
       
     }
   
-  names(outLoc) <- names(data)
+  names(outLoc) <- names(data) #Naming the location of the list as the same as the variable names of the data frame.
   
-  return(noquote(outLoc))
+  return(noquote(outLoc))   #noquote() function is used to remove quote when displaying a character variable.
 }
